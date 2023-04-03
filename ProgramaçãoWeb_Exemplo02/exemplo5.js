4142a84f4830cfcd7ceeda6d0c28902e208a3aea
Brunno Nascimento <brunnonascimento011@gmail.com>
2023-04-03 10:05:36 -0300

    Programação Web Exemplos

---
+++ ProgramaçãoWeb_Exemplo02/exemplo5.js
@@ -0,0 +1,14 @@
+let num1;
+let num2;
+let resultado;
+
+//num = 10;
+num1 = Number(prompt("Digite o valor do primeiro número: "))
+//num2 = 20;
+num2 = Number(prompt("Digite o valor do segundo número: "))
+
+
+resultado = num1 + num2;
+//console.log(resultado); // impressão no terminal
+alert(resultado);
+//concatenação: juntar um texto com variáveis etc.
